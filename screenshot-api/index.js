854701b28054543fe48fa4596a82ec1cf8f9f74b
sridhar02 <kattasridhar02@gmail.com>
2024-02-05 14:39:37 +0530

    screenshot api

--- screenshot-api/index.js
+++ screenshot-api/index.js
@@ -16,28 +16,42 @@ app.use(morgan("combined"));
 //   })
 // );
 
-const takeScreenshot = async (url) => {
+const takeScreenshot = async ({ url, options }) => {
+  const {
+    fullPage,
+    deviceScaleFactor,
+    width,
+    height,
+    delay,
+    type,
+    omitBackground,
+  } = options;
+
   const browser = await puppeteer.launch({
     headless: "new",
-    executablePath: "/usr/bin/chromium-browser",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    ignoreDefaultArgs: ["--disable-extensions"],
   });
   const page = await browser.newPage();
 
+  await page.waitForTimeout(delay);
+
   await page.setViewport({
-    width: 1920,
-    height: 1080,
-    deviceScaleFactor: 1,
+    width,
+    height,
+    deviceScaleFactor,
   });
 
   await page.goto(url, {
-    waitUntil: "networkidle2",
+    // waitUntil: "networkidle2",
+    waitUntil: ["load", "domcontentloaded"],
   });
+
+  await scroll(page);
+
   const screenshot = await page.screenshot({
-    // fullPage: true,
-    type: "jpeg",
+    fullPage,
+    type,
     quality: 100,
+    omitBackground,
   });
   await browser.close();
   return screenshot;
@@ -48,12 +62,32 @@ app.get("/status", (req, res) => {
 });
 
 app.get("/screenshot", async (req, res) => {
-  const { url } = req.query;
+  const {
+    url,
+    fullPage = false,
+    deviceScaleFactor = 1,
+    width = 1920,
+    height = 1080,
+    delay = 0,
+    type = "jpeg",
+    omitBackground = false,
+  } = req.query;
+
+  const options = {
+    fullPage,
+    deviceScaleFactor,
+    width,
+    height,
+    delay,
+    type,
+    omitBackground,
+  };
+
   if (!url) {
     return res.status(400).send("URL is required");
   }
   try {
-    const screenshot = await takeScreenshot(url);
+    const screenshot = await takeScreenshot({ url, options });
     res.setHeader("content-Type", "image/jpeg");
     res.send(screenshot);
   } catch (error) {
@@ -61,6 +95,24 @@ app.get("/screenshot", async (req, res) => {
     res.status(500).send({ message: "Error generating screenshot", error });
   }
 });
+
+async function scroll(page) {
+  return await page.evaluate(async () => {
+    return await new Promise((resolve, reject) => {
+      var i = setInterval(() => {
+        window.scrollBy(0, window.innerHeight);
+        if (
+          document.scrollingElement.scrollTop + window.innerHeight >=
+          document.scrollingElement.scrollHeight
+        ) {
+          window.scrollTo(0, 0);
+          clearInterval(i);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}
 
 app.listen(port, () => {
   console.log(`server is running at port ${port}`);
